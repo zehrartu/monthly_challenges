96d29e1724ff6ca7e3a2d6460bb4a44b853c0acd
Zehra <zehra.agdeniz@edu.rtu.lv>
2025-02-24 11:24:00 +0200

    improving by adding dynamic properties

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges ={
     "january":"Eat no meat in the entire month!",
@@ -23,7 +24,8 @@ def monthly_challenge_by_number(request, month):
     if month> len(months):
         return HttpResponseNotFound("This month is not valid!")
     redirect_month = months[month-1]
-    return HttpResponseRedirect("/challenges/"+redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
